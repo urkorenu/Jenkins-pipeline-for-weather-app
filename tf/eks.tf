1269bdf0c17528f367614c031e418b3664061d30
Ur Koren <ork14790@gmail.com>
2024-10-30 15:00:37 +0200

    REmoved ebs form tf

--- tf/eks.tf
+++ tf/eks.tf
@@ -1,29 +1,58 @@
+# Define the EKS Cluster IAM Role
 resource "aws_iam_role" "eks" {
   name = "${local.env}-${local.eks-cluster-name}-eks-cluster"
 
   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [{
       Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Effect  = "Allow"
       Principal = {
         Service = "eks.amazonaws.com"
       }
     }]
-    Version = "2012-10-17"
   })
 }
 
-
-resource "aws_iam_role_policy_attachment" "eks" {
+# Attach the AmazonEKSClusterPolicy to the EKS Cluster IAM Role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks.name
 }
 
+
+# Define the EKS Node Group IAM Role
+resource "aws_iam_role" "eks_node_group" {
+  name = "${local.env}-${local.eks-cluster-name}-node-group"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect  = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach necessary policies to the EKS Node Group IAM Role
+resource "aws_iam_role_policy_attachment" "node_group_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+# Define the EKS Cluster
 resource "aws_eks_cluster" "eks" {
   name     = "${local.env}-${local.eks-cluster-name}"
   version  = local.eks_version
   role_arn = aws_iam_role.eks.arn
-
 
   vpc_config {
     endpoint_private_access = false
@@ -35,11 +64,5 @@ resource "aws_eks_cluster" "eks" {
     ]
   }
 
-  #access_config {
-  #authentication_mode                         = "API"
-  #bootstrap_cluster_creator_admin_permissions = true
-  #}
-
-  depends_on = [aws_iam_role_policy_attachment.eks]
 }
 
